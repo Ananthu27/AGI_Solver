39ddba1146d9326f42094cdfef8e28620f6d0e12
Ananthu <kma270999@gmail.com>
2020-05-14 02:48:41 +0530

    added basic implementatoin of simple and steepest hill climber not tested

--- agis.h
+++ agis.h
@@ -1,30 +1,75 @@
 #include <iostream>
+#include <vector>
+#include <time.h>
+// #include <limits>
 
 using namespace std;
 
 template <typename dtype>
 
-class test
+class agis
 {
 public:
-    dtype goal, current, next;
+    agis() {}
+    ~agis() {}
 
-    test() {}
-    test(dtype g, dtype c, dtype n)
+    int shc(dtype (*apply)(int), float (*heuristic)(dtype), int max_rule, dtype current, dtype goal)
     {
-        goal = g;
-        current = c;
-        next = n;
+        int rule = -1;
+
+        if (current.exist() && heuristic(current) != 0)
+        {
+            try
+            {
+                vector<int> rule_applied = vector<int>(max_rule, 0);
+                int rand_rule = rand() % max_rule + 1;
+                srand(time(0));
+                for (int i = 1; i < max_rule; i++)
+                {
+                    while (rand_rule > 0 && rand_rule <= max_rule && rule_applied[rand_rule] == 1)
+                        rand_rule = rand() % max_rule + 1;
+                    rule_applied[rand_rule] = 1;
+
+                    dtype next = apply(rand_rule);
+
+                    if (next.exist() && heuristic(current) > heuristic(next))
+                    {
+                        rule = rand_rule;
+                        break;
+                    }
+                }
+            }
+            catch (exception e)
+            {
+                cout << " exception in shc" << endl;
+            }
+        }
+
+        return rule;
     }
-    ~test() {}
 
-    void display()
+    int sthc(dtype (*apply)(int), float (*heuristic)(dtype), int max_rule, dtype current, dtype goal)
     {
-        cout << goal << endl;
-        cout << current << endl;
-        cout << next << endl;
+        int rule = -1;
+
+        if (current.exist() && heuristic(current) != 0)
+        {
+            try
+            {
+                float min = heuristic(current);
+                for (int r = 1; r <= max_rule; r++)
+                {
+                    dtype next = apply(r);
+                    if (next.exist() && heuristic(next) < min)
+                        rule = r;
+                }
+            }
+            catch (exception e)
+            {
+                cout << "exception in sthc " << endl;
+            }
+        }
+
+        return rule;
     }
 };
-int main()
-{
-}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "agis.h"
+
+int main()
+{
+
+    return 0;
+}
